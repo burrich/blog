1f63f9054789aa4755640c38df3cceb6e8e1d679
burrich <nideto@gmail.com>
2014-12-09 20:20:22 +0100

    Blog bundle

---
+++ src/Burrich/BlogBundle/BurrichBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Burrich\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BurrichBlogBundle extends Bundle
+{
+}
---
+++ src/Burrich/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Burrich\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array(
+                // ...
+        );    
+    }
+
+}
